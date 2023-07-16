b9c57d2828d96d165145e7729a34ee2b4e87e5a9
Aodj05 <aodj05@msn.com>
2023-07-16 17:00:18 -0400

    Added power ups and Game over logic

--- pyBreakout.py
+++ pyBreakout.py
@@ -1,4 +1,5 @@
 import pygame
+import random
 
 # Initialize Pygame
 pygame.init()
@@ -7,7 +8,6 @@ pygame.mixer.init()
 
 paddle_collision = pygame.mixer.Sound("sounds/Blip_Select7.wav")
 brick_collision = pygame.mixer.Sound("sounds/Explosion6.wav")
-
 
 # Define the game window dimensions
 width = 800
@@ -20,6 +20,8 @@ screen = pygame.display.set_mode((width, height))
 RED = (255, 0, 0)
 BLUE = (0, 0, 255)
 WHITE = (255, 255, 255)
+YELLOW = (255, 255, 0)
+GREEN = (0, 255, 0)
 
 class Paddle:
     def __init__(self):
@@ -31,6 +33,8 @@ class Paddle:
         self.direction = None
         self.move_delay = 10
         self.last_move_time = pygame.time.get_ticks()
+        self.power_up_enlarged = False
+        self.enlarge_timer = 0
 
     def draw(self):
         pygame.draw.rect(screen, BLUE, (self.x, self.y, self.width, self.height))
@@ -48,11 +52,26 @@ class Paddle:
                 self.x = width - self.width
             self.last_move_time = current_time
 
+        if self.power_up_enlarged:
+            self.enlarge_timer -= 1
+            if self.enlarge_timer <= 0:
+                self.power_up_enlarged = False
+                self.width = 100
+
     def move_left(self):
         self.direction = "left"
 
     def move_right(self):
         self.direction = "right"
+
+    def enlarge_paddle(self):
+        self.power_up_enlarged = True
+        self.width = 200
+        self.enlarge_timer = 2
+
+    def increase_speed(self):
+        self.speed += 2
+        pygame.time.set_timer(SPEED_POWERUP_EVENT, 3000)
 
 class Ball:
     def __init__(self):
@@ -61,18 +80,29 @@ class Ball:
         self.y = height // 2
         self.speed_x = .2
         self.speed_y = -.2
+        self.power_up_explosion = False
+        self.explosion_timer = 0
 
     def draw(self):
         pygame.draw.circle(screen, RED, (self.x, self.y), self.radius)
 
     def update(self):
-            self.x += self.speed_x
-            self.y += self.speed_y
+        self.x += self.speed_x
+        self.y += self.speed_y
 
-            if self.x - self.radius < 0 or self.x + self.radius > width:
-                self.speed_x = -self.speed_x
-            if self.y - self.radius < 0 or self.y + self.radius > height:
-                self.speed_y = -self.speed_y
+        if self.x - self.radius < 0 or self.x + self.radius > width:
+            self.speed_x = -self.speed_x
+        if self.y - self.radius < 0:
+            self.speed_y = -self.speed_y
+        elif self.y + self.radius > height:
+            self.reset_ball()
+            global lives
+            lives -= 1
+
+        if self.power_up_explosion:
+            self.explosion_timer -= 1
+            if self.explosion_timer <= 0:
+                self.power_up_explosion = False
 
     def handle_collision(self, bricks):
         # Collision detection with paddle
@@ -85,46 +115,112 @@ class Ball:
         # Collision detection with bricks
         ball_rect = pygame.Rect(self.x - self.radius, self.y - self.radius, self.radius * 2, self.radius * 2)
         bricks_to_remove = []
+        power_up_effects = []
         for brick in bricks:
             if brick.colliderect(ball_rect):
                 bricks_to_remove.append(brick)
                 brick_collision.play()
+                if isinstance(brick, PowerUpBrick):
+                    power_up_effects.append(brick.power_up_effect)
 
         for brick in bricks_to_remove:
-            bricks.remove(brick)
-            self.speed_x = self.speed_y = -self.speed_y
+            if not isinstance(brick, PowerUpBrick):
+                bricks.remove(brick)
 
-class Bricks:
-    def __init__(self):
+        for effect in power_up_effects:
+            effect(paddle, self)
+
+        if not bricks:
+            # All bricks cleared, game over
+            global game_over
+            game_over = True
+
+    def reset_ball(self):
+        self.x = width // 2
+        self.y = height // 2
+        self.speed_x = .2
+        self.speed_y = -.2
+
+class Brick:
+    def __init__(self, x, y, color):
         self.width = 50
         self.height = 20
-        self.spacing = 10
-        self.rows = 5
-        self.cols = (width - self.spacing) // (self.width + self.spacing)
-        self.bricks = []
-        for row in range(self.rows):
-            for col in range(self.cols):
-                brick_x = col * (self.width + self.spacing) + self.spacing
-                brick_y = row * (self.height + self.spacing) + self.spacing
-                self.bricks.append(pygame.Rect(brick_x, brick_y, self.width, self.height))
+        self.x = x
+        self.y = y
+        self.color = color
 
     def draw(self):
-        for brick in self.bricks:
-            pygame.draw.rect(screen, WHITE, brick)
+        pygame.draw.rect(screen, self.color, (self.x, self.y, self.width, self.height))
 
-    def handle_collision(self, ball):
-        for brick in self.bricks:
-            if brick.colliderect(ball):
-                self.bricks.remove(brick)
-                ball.speed_y = -ball.speed_y
+    def colliderect(self, other_rect):
+        return pygame.Rect(self.x, self.y, self.width, self.height).colliderect(other_rect)
+
+class PowerUpBrick(Brick):
+    def __init__(self, x, y, color):
+        super().__init__(x, y, color)
+        self.power_up_effect = None
+
+class EnlargePowerUpBrick(PowerUpBrick):
+    def __init__(self, x, y):
+        super().__init__(x, y, YELLOW)
+        self.power_up_effect = self.handle_power_up
+
+    def handle_power_up(self, paddle, ball):
+        paddle.enlarge_paddle()
+
+class SpeedPowerUpBrick(PowerUpBrick):
+    def __init__(self, x, y):
+        super().__init__(x, y, GREEN)
+        self.power_up_effect = self.handle_power_up
+
+    def handle_power_up(self, paddle, ball):
+        paddle.increase_speed()
+
+class ExplosionPowerUpBrick(PowerUpBrick):
+    def __init__(self, x, y):
+        super().__init__(x, y, BLUE)
+        self.power_up_effect = self.handle_power_up
+
+    def handle_power_up(self, paddle, ball):
+        ball.handle_collision(bricks)
 
 # Create game objects
 paddle = Paddle()
 ball = Ball()
-bricks = Bricks()
+bricks = []
+
+# Generate regular bricks
+brick_width = 50
+brick_height = 20
+brick_spacing = 10
+
+for row in range(5):
+    for col in range(16):
+        brick_x = col * (brick_width + brick_spacing) + brick_spacing
+        brick_y = row * (brick_height + brick_spacing) + brick_spacing
+        bricks.append(Brick(brick_x, brick_y, WHITE))
+
+# Generate power-up bricks
+power_up_brick_count = 3
+power_up_brick_types = [EnlargePowerUpBrick, SpeedPowerUpBrick, ExplosionPowerUpBrick]
+power_up_brick_colors = [YELLOW, GREEN, BLUE]
+
+for row in range(5):
+    for col in range(power_up_brick_count):
+        brick_x = col * (brick_width + brick_spacing) + brick_spacing
+        brick_y = row * (brick_height + brick_spacing) + brick_spacing
+        brick_type = random.choice(power_up_brick_types)
+        brick_color = random.choice(power_up_brick_colors)
+        bricks.append(brick_type(brick_x, brick_y))
 
 # Game loop
 running = True
+lives = 3
+game_over = False
+
+# Define custom events
+SPEED_POWERUP_EVENT = pygame.USEREVENT + 1
+
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -134,28 +230,76 @@ while running:
                 paddle.move_left()
             elif event.key == pygame.K_RIGHT:
                 paddle.move_right()
+            elif event.key == pygame.K_q and game_over:
+                running = False
+            elif event.key == pygame.K_r and game_over:
+                # Reset game
+                paddle = Paddle()
+                ball = Ball()
+                bricks = []
+
+                # Generate regular bricks
+                for row in range(5):
+                    for col in range(16):
+                        brick_x = col * (brick_width + brick_spacing) + brick_spacing
+                        brick_y = row * (brick_height + brick_spacing) + brick_spacing
+                        bricks.append(Brick(brick_x, brick_y, WHITE))
+
+                # Generate power-up bricks
+                for row in range(5):
+                    for col in range(power_up_brick_count):
+                        brick_x = col * (brick_width + brick_spacing) + brick_spacing
+                        brick_y = row * (brick_height + brick_spacing) + brick_spacing
+                        brick_type = random.choice(power_up_brick_types)
+                        brick_color = random.choice(power_up_brick_colors)
+                        bricks.append(brick_type(brick_x, brick_y))
+
+                lives = 3
+                game_over = False
         elif event.type == pygame.KEYUP:
             if event.key == pygame.K_LEFT and paddle.direction == "left":
                 paddle.direction = None
             elif event.key == pygame.K_RIGHT and paddle.direction == "right":
                 paddle.direction = None
+        elif event.type == SPEED_POWERUP_EVENT:
+            paddle.speed -= 2
+            pygame.time.set_timer(SPEED_POWERUP_EVENT, 0)
 
-    # Update paddle direction
-    paddle.update()
+    if not game_over:
+        # Update paddle direction
+        paddle.update()
 
-    # Update the ball position
-    ball.update()
+        # Update the ball position
+        ball.update()
 
-    # Clear the screen
-    screen.fill((0, 0, 0))
+        # Clear the screen
+        screen.fill((0, 0, 0))
 
-    # Draw game objects
-    paddle.draw()
-    ball.draw()
-    bricks.draw()
+        # Draw game objects
+        paddle.draw()
+        ball.draw()
+        for brick in bricks:
+            brick.draw()
 
-    # Handle ball-to-brick collision detection
-    ball.handle_collision(bricks.bricks)
+        # Handle ball-to-brick collision detection
+        ball.handle_collision(bricks)
+
+        if lives <= 0:
+            game_over = True
+
+        if game_over:
+            # Game over
+            font = pygame.font.Font(None, 42)
+            game_over_text = font.render("Game Over", True, RED)
+            screen.blit(game_over_text, (width // 2 - game_over_text.get_width() // 2, height // 2))
+
+            # Restart or Quit prompt
+            prompt_text = font.render("Press R to Restart or Q to Quit", True, RED)
+            screen.blit(prompt_text, (width // 2 - prompt_text.get_width() // 2, height // 2 + 50))
+    else:
+        # Update the display
+        pygame.display.flip()
+        continue
 
     # Update the display
     pygame.display.flip()
